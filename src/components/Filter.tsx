42227f4420c05e33cced4d4ea178767e5695378f
Bruno Santos <>
2023-06-21 11:13:42 +0100

    chore: add custom hook to get input type

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,29 +1,52 @@
 import { useContext } from 'react';
 import Select, { SingleValue } from 'react-select';
-import { properties } from '../store';
+import { operators, properties } from '../store';
 import useFindPossibleOperators from '../hooks/useFindPossibleOperators';
 import formatPropertyToDropdownObject from '../utils/formatPropertyToDropdownObject';
 import { FilterContext } from '../context/FilterContext';
+import { OperatorId } from '../types';
+import useGetInputType from '../hooks/useGetInputType';
 
-type Option = {
+type PropertyOption = {
   id: number;
   value: string;
   label: string;
 };
 
+type OperatorOption = {
+  id: OperatorId;
+  value: string;
+  label: string;
+};
+
 const Filter = () => {
-  const { propertyFilter, setPropertyFilter } = useContext(FilterContext);
+  const {
+    propertyFilter,
+    setPropertyFilter,
+    setOperatorFilter,
+    operatorFilter,
+  } = useContext(FilterContext);
 
   const propertyOptions = formatPropertyToDropdownObject(properties);
   const operatorOptions = useFindPossibleOperators(propertyFilter);
   const shouldShowOperatorDropdown = propertyFilter && operatorOptions;
 
-  function handlePropertyChange(e: SingleValue<Option>) {
-    const selectedProperty = properties.find(
-      (property) => property.id === e?.id
-    );
+  const input = useGetInputType({
+    propertyFilter,
+    operatorFilter,
+  });
 
+  const shouldRenderInput =
+    input && input.needsInput && input.inputType && operatorFilter;
+
+  function handlePropertyChange(e: SingleValue<PropertyOption>) {
+    const selectedProperty = properties.find((prop) => prop.id === e?.id);
     setPropertyFilter(selectedProperty);
+  }
+
+  function handleOperatorChange(e: SingleValue<OperatorOption>) {
+    const selectedOperator = operators.find((op) => op.id === e?.id);
+    setOperatorFilter(selectedOperator);
   }
 
   return (
@@ -35,9 +58,10 @@ const Filter = () => {
       {shouldShowOperatorDropdown && (
         <label>
           Operator
-          <Select options={operatorOptions} />
+          <Select onChange={handleOperatorChange} options={operatorOptions} />
         </label>
       )}
+      {shouldRenderInput && <input type={input.inputType} />}
     </form>
   );
 };
