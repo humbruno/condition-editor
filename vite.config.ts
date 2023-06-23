363315289d4fd87e2e7baeeb7c1becb59b70fa35
Bruno Santos <>
2023-06-23 16:04:20 +0100

    fix: update package.json homepage url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/condition-editor',
   plugins: [react()],
   build: {
     outDir: './build',
